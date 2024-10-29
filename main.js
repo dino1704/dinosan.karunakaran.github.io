d12f89314dbac72e52d3543cced0cde369a7be33
dino1704 <dinosan.karunakaran@torontomu.ca>
2024-10-29 13:21:00 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -28,3 +28,31 @@ function myFunction() {
       }
     }
   }
+
+  function submitForm() {
+    // Getting values from each form field
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const comments = document.getElementById('comments').value;
+    const satisfaction = document.querySelector('input[name="satisfaction"]:checked').value;
+    const subscribe = document.querySelector('input[name="subscribe"]').checked;
+    const country = document.getElementById('country').value;
+    const date = document.getElementById('date').value;
+    const age = document.getElementById('age').value;
+
+    // Creating an object to store form data
+    const formData = {
+        name,
+        email,
+        comments,
+        satisfaction,
+        subscribe,
+        country,
+        date,
+        age,
+    };
+
+    // Saving the form data object in local storage
+    localStorage.setItem('formData', JSON.stringify(formData));
+    alert('Your feedback has been submitted!');
+}
